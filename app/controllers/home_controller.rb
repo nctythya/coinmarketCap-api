68b41d552dbf91703af93409fb106613eee8c107
chhomtythya norong <nctythya@yahoo.com>
2022-12-18 22:12:34 +1100

    stage 4 done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,22 @@
+require "json"
+require "open-uri"
+
 class HomeController < ApplicationController
   def index
   end
 
   def about
+  end
+
+  def lookup
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{ENV['API_KEY']}"
+    #url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=9da0f106-d8b3-4138-865c-09975e3c1f58"
+    uri = URI.open(url).read
+    @tickers = JSON.parse(uri)
+
+    @symbol = params[:query]
+    if @symbol
+      @symbol = @symbol.upcase
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "about", to: "home#about"
+  get "lookup", to: "home#lookup"
+  post "lookup", to: "home#lookup"
   root to: "home#index"
   #root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
