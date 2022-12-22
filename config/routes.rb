d0d93a71a6d5b366be86375b9fda76e68e229de9
chhomtythya norong <nctythya@yahoo.com>
2022-12-22 16:02:15 +1100

    form and devise fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   get "about", to: "home#about"
+  get "welcome", to: "home#index"
   get "lookup", to: "home#lookup"
   post "lookup", to: "home#lookup"
-  root to: "home#index"
+  root to: "pages#home"
   #root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
